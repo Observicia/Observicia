3a6b2c6abc8f543f55d0a82cbd65b80a8335bd4c
Huamin Chen <hchen@redhat.com>
2024-12-15 10:20:20 -0500

    feat(llm): support ollama
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- sdk/observicia/patchers/__init__.py
+++ sdk/observicia/patchers/__init__.py
@@ -6,15 +6,17 @@ from .openai import OpenAIPatcher
 from .anthropic import AnthropicPatcher
 from .litellm import LiteLLMPatcher
 from .watsonx import WatsonxPatcher
+from .ollama import OllamaPatcher
 
 DEFAULT_PATCHERS = {
     "openai": OpenAIPatcher,
     "anthropic": AnthropicPatcher,
     "litellm": LiteLLMPatcher,
-    "watsonx": WatsonxPatcher
+    "watsonx": WatsonxPatcher,
+    "ollama": OllamaPatcher
 }
 
 __all__ = [
     "OpenAIPatcher", "AnthropicPatcher", "LiteLLMPatcher", "WatsonxPatcher",
-    "DEFAULT_PATCHERS"
+    "OllamaPatcher", "DEFAULT_PATCHERS"
 ]
